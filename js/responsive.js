e086d003fa732e5ee49ba202d690880a77d44349
JacProsser <jacleighp@gmail.com>
2021-06-19 17:11:42 +0100

    new website

--- js/responsive.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navbar = document.getElementsByClassName("collapse")[0]
-
-toggleButton.addEventListener("click", () => {
-    navbar.classList.toggle("active")
-})
